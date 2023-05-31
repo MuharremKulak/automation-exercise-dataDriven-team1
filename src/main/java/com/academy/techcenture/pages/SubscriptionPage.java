7fef39df73a7e683ea14872df40c60838e7b0e89
pelinozgur <132114823+pelinozgur@users.noreply.github.com>
2023-06-01 01:31:56 +0300

    Update SubscriptionPage.java

--- src/main/java/com/academy/techcenture/pages/SubscriptionPage.java
+++ src/main/java/com/academy/techcenture/pages/SubscriptionPage.java
@@ -1,4 +1,57 @@
 package com.academy.techcenture.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
 public class SubscriptionPage {
+
+    public SubscriptionPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+
+    private WebDriver driver;
+
+    @FindBy(xpath="//li/a/i[@class='fa fa-home']")
+    public WebElement HomePageVisible;
+
+    @FindBy(xpath ="//div[@class='single-widget']//h2 ")
+    public WebElement SubsHomePage;
+
+    @FindBy(xpath = "//form/input[2][contains(@type,'email')]")
+    public WebElement EnterSubsEmail;
+
+    @FindBy(xpath="//form/button[@id='subscribe']")
+    public WebElement SubsButton;
+
+    @FindBy(xpath = "//div[contains(text(),'You have been successfully subscribed!')]")
+    public WebElement SuccessSubsText;
+
+
+public void verifyHomePageVisible(){
+    HomePageVisible.click();
+    Assert.assertTrue(HomePageVisible.isDisplayed());
+
+}
+    public void verifySubscriptionHomePage(){
+        Assert.assertTrue(SubsHomePage.isDisplayed());
+    }
+
+    public void enterEmailForSubs(){
+    EnterSubsEmail.sendKeys("lee@gmail.com");
+    }
+
+    public void clickSubsButton(){
+    SubsButton.click();
+    }
+
+    public void verifySubsMessage(){
+        Assert.assertTrue(SuccessSubsText.isDisplayed());
+
+
+    }
+
 }
